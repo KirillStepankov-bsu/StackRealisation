ad40c83890a1a7bf57d6d88c880c4142ab669b5c
Kirill Stepankov <fpm.stepanko@bsu.by>
2021-12-06 21:33:43 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+template<typename T>
+class Stack {
+public:
+    Stack() : size(0) { data = new T; }
+
+    [[maybe_unused]] void push(int element) { data[size++] = element; }
+
+    T pop() { return data[--size]; }
+
+    [[nodiscard]] int length() const { return size; }
+
+    [[nodiscard]] T top() const { return data[size - 1]; }
+
+    [[nodiscard]] bool isEmpty() const { return size == 0; }
+
+    ~Stack() {
+        delete[] data;
+        data = nullptr;
+    }
+
+private:
+    T *data;
+    int size;
+};
