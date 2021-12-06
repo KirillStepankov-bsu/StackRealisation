637343180e7ca9800c501ebce6cfea914d86f826
Kirill Stepankov <fpm.stepanko@bsu.by>
2021-12-06 22:17:38 +0300

    second commit

--- stackarray.cpp
+++ stackarray.cpp
@@ -3,7 +3,7 @@ class Stack {
 public:
     Stack() : size(0) { data = new T; }
 
-    void push(int element) { data[size++] = element; }
+    void push(const T& element) { data[size++] = element; }
 
     T pop() { return data[--size]; }
 
